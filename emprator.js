8c498d7e9719d0b855a59b457652cd446c34eb2f
discordbot827 <86576491+discordbot827@users.noreply.github.com>
2022-06-18 00:21:28 +0300

    Update emprator.js

--- emprator.js
+++ emprator.js
@@ -35,7 +35,7 @@ const cmd = require("node-cmd");
 const prefix = "%%";
 const cooldown = new Set();
 const cdtime = 7;
-client.login("ODU5ODU0ODUyNjUzNTgwMjg5.G1jp5Z.yvirlmQlRD5X7Yl1FJVsZcgeGE5cgonEIUTgC8")
+client.login("ODU5ODU0ODUyNjUzNTgwMjg5.GZhPf3.WNB36EywsM9Ke4ceEIawNHWBO38zKBG5TSH_So")
 client.on("ready", () => {
   console.log(`${client.user.tag}`);
   console.log(`Guilds: ${client.guilds.cache.size}`);
