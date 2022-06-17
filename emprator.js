fe1d4a0afa1987f177acc9bd00d459252a0c277b
discordbot827 <86576491+discordbot827@users.noreply.github.com>
2022-06-17 23:46:36 +0300

    Update emprator.js

--- emprator.js
+++ emprator.js
@@ -48,11 +48,7 @@ client.on("ready", () => {
 });
 client.on("warn", info => console.log(info));
 client.on("error", console.error);
-
-/////////////
-client.on("guildCreate", guild => { let channel = client.channels.cache.get("987446508515393587"); let embed = new MessageEmbed().setColor("#FF0000") .setAuthor(client.user.username, client.user.avatarURL()) .setTitle( `✅ Join Server`) .addField(" **Server Name**", `${guild.name}`) .addField(" **Server Owner**", `${guild.owner}`) .addField(" **Server Id**", `${guild.id}`) .addField(" **Member Count**", `${guild.memberCount}`) .setFooter(`${client.user.tag}`); channel.send(embed);}); client.on("guildDelete", guild => { let channel = client.channels.cache.get("987446508515393587"); let embed = new MessageEmbed() .setColor("#FF0000") .setAuthor(client.user.username, client.user.avatarURL()) .setTitle( `❌ Left Server`) .addField(" **Server Name**", `${guild.name}`) .addField(" **Server Owner**", `${guild.owner}`) .addField(" **Server Id**", `${guild.id}`) .addField(" **Member Count**", `${guild.memberCount}`) .setFooter(`${client.user.tag}`); channel.send(embed);});
 //////
-
 client.on("message", async message => {
   if (message.content.startsWith(prefix + "help")) {
     if (cooldown.has(message.author.id)) {
